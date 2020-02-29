d6db03540f5cce900af15c13cb3a4535d979e735
Taras Tarlov <ttarlov@gmail.com>
2020-02-29 15:51:18 -0700

    Add myTrips property

--- src/traveler.js
+++ src/traveler.js
@@ -1,26 +1,9 @@
-// const User = require('./user');
-
-// class Traveler extends User {
-//   constructor(id, name, travelerType, apiRequestController) {
-//     super(apiRequestController)
-//     this.id = id;
-//     this.name = name;
-//     this.travelerType = travelerType;
-//   }
-//
-//
-//   getMyTrips() {
-//
-//   }
-//
-// }
-
-
 class Traveler {
-  constructor(data) {
+  constructor(data, myTripsData) {
     this.id = data.id;
     this.name = data.name;
     this.travelerType = data.travelerType;
+    this.myTrips = myTripsData
   }
 
 
