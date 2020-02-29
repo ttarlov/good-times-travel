b8217705e986a528a11f23d5fb6bf4fb91605855
Taras Tarlov <ttarlov@gmail.com>
2020-02-29 12:10:06 -0700

    structure class for Agency

---
+++ src/travel-agent.js
@@ -0,0 +1,9 @@
+class Agency {
+  constructor(tripsData, destinationsData, listOfTravelers) {
+    this.trips = tripsData;
+    this.destinations = destinationsData;
+    this.travelers = listOfTravelers;
+  }
+}
+
+module.exports = Agency
