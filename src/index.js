d7117597515a45af7b22598ceaedbd71c7b2c002
Taras Tarlov <ttarlov@gmail.com>
2020-02-29 15:53:28 -0700

    Create fuction to instantiate traveler and trips fetching data

--- src/index.js
+++ src/index.js
@@ -6,27 +6,27 @@ import './images/hammer-sickle.png';
 import Traveler from './traveler';
 import ApiRequestController from './api-controller';
 import Agency from './travel-agent';
+import Trips from './trips';
 
 const api = new ApiRequestController();
+
 console.log(api);
 
 const generateUserId = () => {
   let userName = $('#username-input').val();
   let userId = userName.match(/\d+/g);
-  return userId;
+  return Number(userId);
 }
 
 
  function processLogIn(data) {
   if($('#password-input').val() === 'travel2020' && $('#username-input').val().includes('travel'))  {
     event.preventDefault();
-    // createUser(data);
-    api.getUserById(generateUserId()).then((data) => createTraveler(data));
+    getTravelerData()
     domUpdates.hideLoginWindow();
   } else if ($('#password-input').val() === 'travel2020' && $('#username-input').val().includes('agency')) {
     event.preventDefault();
     getAgencyData();
-    // api.fetchAllTrips().then((tripsData) => createAgency(tripsData)).catch(error => console.log(error.message));
     domUpdates.hideLoginWindow();
   } else {
     window.alert("Wrong Password or User Name")
@@ -34,10 +34,23 @@ const generateUserId = () => {
 };
 
 
+const getTravelerData = () => {
+  let fethechedTreveler = api.getUserById(generateUserId());
+  let fethchedAllTrips = api.fetchAllTrips();
 
-const createTraveler = (data) => {
-  let user = new Traveler(data);
-console.log(user)
+  Promise.all([fethechedTreveler, fethchedAllTrips])
+  .then(finalVals => {
+    let traveler = finalVals[0];
+    let allTrips = finalVals[1];
+    createTraveler(traveler, allTrips)
+  }).catch(error => console.log(error.message))
+};
+
+
+const createTraveler = (traveler, allTrips) => {
+  let trips = new Trips(allTrips);
+  let loggedInTraveler = new Traveler(traveler, trips.getTripsById(generateUserId()));
+  console.log(loggedInTraveler)
 };
 
 
@@ -53,7 +66,6 @@ const getAgencyData = () => {
     let travelersData = finalVals[2];
     createAgency(tripsData, destinationData, travelersData);
   }).catch(error => console.log(error.message))
-
 };
 
 
@@ -65,5 +77,5 @@ const createAgency = (tripsData, destinationData, travelersData) => {
   console.log(agent)
 }
 
-// $("body").click(eventHandler);
+
 $('#log-in-btn').click(processLogIn)
