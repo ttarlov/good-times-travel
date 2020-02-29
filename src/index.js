3209218d8f3d59e95b787d7ec8b5c8a6d4129589
Taras Tarlov <ttarlov@gmail.com>
2020-02-29 12:12:05 -0700

    Add async function to process fetch data for Agency

--- src/index.js
+++ src/index.js
@@ -5,9 +5,10 @@ import './images/hammer-sickle.png';
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import Traveler from './traveler';
 import ApiRequestController from './api-controller';
+import Agency from './travel-agent';
 
 const api = new ApiRequestController();
-
+console.log(api);
 
 const generateUserId = () => {
   let userName = $('#username-input').val();
@@ -18,23 +19,51 @@ const generateUserId = () => {
 
  function processLogIn(data) {
   if($('#password-input').val() === 'travel2020' && $('#username-input').val().includes('travel'))  {
-    createUser(data);
-    api.getUserById(generateUserId()).then((data) => createUser(data));
+    event.preventDefault();
+    // createUser(data);
+    api.getUserById(generateUserId()).then((data) => createTraveler(data));
+    domUpdates.hideLoginWindow();
+  } else if ($('#password-input').val() === 'travel2020' && $('#username-input').val().includes('agency')) {
+    event.preventDefault();
+    getAgencyData();
+    // api.fetchAllTrips().then((tripsData) => createAgency(tripsData)).catch(error => console.log(error.message));
     domUpdates.hideLoginWindow();
   } else {
-    alert("Wrong Password or User Name! User name must be travelxx where xx is user id");
+    window.alert("Wrong Password or User Name")
   }
 };
 
 
 
-const createUser = (data) => {
+const createTraveler = (data) => {
   let user = new Traveler(data);
 console.log(user)
 };
 
 
+const getAgencyData = () => {
+ let fetchedTripsData = api.fetchAllTrips();
+ let fetchedDestinationData = api.fetchAllDestinations();
+ let fetchedTravelers = api.getAllTravelers();
 
+ Promise.all([fetchedTripsData, fetchedDestinationData, fetchedTravelers])
+  .then(finalVals => {
+    let tripsData = finalVals[0];
+    let destinationData = finalVals[1];
+    let travelersData = finalVals[2];
+    createAgency(tripsData, destinationData, travelersData);
+  }).catch(error => console.log(error.message))
+
+};
+
+
+
+
+
+const createAgency = (tripsData, destinationData, travelersData) => {
+  let agent = new Agency(tripsData, destinationData, travelersData)
+  console.log(agent)
+}
 
 // $("body").click(eventHandler);
 $('#log-in-btn').click(processLogIn)
