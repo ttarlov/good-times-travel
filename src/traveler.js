8e2a4114264d761fae0755433ab198ab4a08cb25
Taras Tarlov <ttarlov@gmail.com>
2020-03-02 14:39:14 -0700

    Add addDestinations,  findPastTrips, findUpcomingTrips methods

--- src/traveler.js
+++ src/traveler.js
@@ -1,17 +1,56 @@
+const moment = require("moment");
+
 class Traveler {
   constructor(data, myTripsData) {
     this.id = data.id;
     this.name = data.name;
     this.travelerType = data.travelerType;
-    this.myTrips = myTripsData
+    this.myTrips = myTripsData;
+    this.pastTrips = this.findPastTrips();
+    this.futureTrips = this.findUpcomingTrips();
+  }
+
+  addDestinations(destinationsArray) {
+    this.myTrips.forEach((trip) => {
+    const tripDestination = destinationsArray.find((destination) => destination.id === trip.destinationID)
+    trip.destination = tripDestination
+    })
+  }
+
+  findPastTrips() {
+    let pastTrips = [];
+    let myTrips = this.myTrips;
+
+    myTrips.forEach(trip => {
+      if(moment(trip.date, "YYYY/MM/DD").fromNow().includes('ago')) {
+        pastTrips.push(trip);
+      }
+    })
+      return pastTrips;
   }
 
 
-  getMyTrips() {
+  findUpcomingTrips() {
+    let futureTrips = []
+    let myTrips = this.myTrips;
 
+    myTrips.forEach(trip => {
+      if(moment(trip.date, "YYYY/MM/DD").fromNow().includes('in')) {
+        futureTrips.push(trip);
+      }
+    })
+    return futureTrips;
   }
 
-}
+
+
+
+
+  getLastName() {
+    return this.name.split(' ')[1];
+  }
+
+};
 
 
 
