536416d03524c0daea21e1cb4c60e76adf395e89
Taras Tarlov <ttarlov@gmail.com>
2020-03-04 08:58:23 -0700

    Add findApprovedTrips, findThisYearsTrips and calculateTotalIncome method

--- src/travel-agent.js
+++ src/travel-agent.js
@@ -4,6 +4,8 @@ class Agency {
     this.trips = tripsData;
     this.travelers = listOfTravelers;
     this.pendingTrips = this.findPendingTrips();
+    this.approvedTrips = this.findApprovedTrips();
+    this.thisYearsTrips = this.findThisYearsTrips()
   }
 
 
@@ -16,6 +18,37 @@ class Agency {
 
   findPendingTrips() {
     return this.trips.filter( trip => trip.status === 'pending')
+  }
+
+  findApprovedTrips() {
+    return this.trips.filter( trip => trip.status === 'approved')
+  }
+
+  findThisYearsTrips() {
+    let tripsThisYear = [];
+    let allTrips = this.trips;
+
+    allTrips.forEach(trip => {
+      let thisYearsTrip = trip.date.split('');
+      if(thisYearsTrip[3] === '0' && trip.status === 'approved') {
+        tripsThisYear.push(trip)
+      }
+    });
+    return tripsThisYear;
+  }
+
+
+  calculateTotalIncome() {
+    console.log(this.thisYearsTrips);
+    let totalRevenue = this.thisYearsTrips.reduce((totalIncome, singleTrip) => {
+      // console.log(totalIncome);
+          totalIncome += ((singleTrip.destination.estimatedLodgingCostPerDay * singleTrip.duration) + (singleTrip.destination.estimatedFlightCostPerPerson * singleTrip.travelers))
+          // console.log(totalIncome)
+          console.log(typeof totalIncome);
+      return totalIncome
+    },0) * .1
+
+    return totalRevenue.toFixed(2)
   }
 
 }
