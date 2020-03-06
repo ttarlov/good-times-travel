fdc1eb6ee4b75e8586e8dc9da8d66b10cc07c259
Taras Tarlov <ttarlov@gmail.com>
2020-03-06 13:47:35 -0700

    Add initial trip test

---
+++ test/trips-test.js
@@ -0,0 +1,10 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+import Trips from '../src/trips';
+
+describe('See if the tests are running', function() {
+  it('should return true', function() {
+    expect(true).to.equal(true);
+  });
+});
