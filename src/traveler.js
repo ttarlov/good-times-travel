6dc36fd283125a7517bf083e353eee9698188a2e
Taras Tarlov <ttarlov@gmail.com>
2020-03-10 11:20:15 -0600

    Add calculatePotentialTripsCost and submitTripRequest Methods

--- src/traveler.js
+++ src/traveler.js
@@ -1,4 +1,6 @@
 const moment = require("moment");
+const ApiRequestController = require('./api-controller')
+
 
 class Traveler {
   constructor(data, myTripsData) {
@@ -9,6 +11,16 @@ class Traveler {
     this.pastTrips = this.findPastTrips(); //need to test for these property changes
     this.futureTrips = this.findUpcomingTrips(); // need to test for these property changes
     this.pendingTrips = this.findPendingTrips();
+    this.tripQuote = {};
+    this.apiController = new ApiRequestController();
+  }
+
+
+  findCurrentTrips() {
+    this.myTrips.filter(trip => {
+      /// needs to find trips that have a date that falls on today. if trip date plus duration includes
+      // today date push trip to the current trips array.
+    })
   }
 
   addDestinations(destinationsArray) {
@@ -63,23 +75,41 @@ class Traveler {
       if(thisYearsTrip[3] === '0' && trip.status === 'approved') {
         tripsThisYear.push(trip)
       }
-      console.log(tripsThisYear);
+      // console.log(tripsThisYear);
     });
       return tripsThisYear.reduce((totalCost, singleTrip) => {
-        console.log(totalCost);
+        // console.log(totalCost);
 
             totalCost += ((singleTrip.destination.estimatedLodgingCostPerDay * singleTrip.duration) + (singleTrip.destination.estimatedFlightCostPerPerson * singleTrip.travelers))
-            console.log(totalCost)
+            // console.log(totalCost)
         return Math.round(totalCost * 1.1);
       },0);
 
   }
 
 
+  calculatePotentialTripCost(allDestinations, potentialTrip) {
+    let mathchedDestination = allDestinations.find( destination => destination.id ===
+      potentialTrip.destinationID)
+    console.log(mathchedDestination)
+    let estimateTripCost = (mathchedDestination.estimatedLodgingCostPerDay *
+      potentialTrip.duration) + (mathchedDestination.estimatedFlightCostPerPerson * potentialTrip.travelers);
+    console.log(estimateTripCost);
+    return Number((estimateTripCost * 1.1).toFixed(2));
+  }
+
 
   getLastName() {
     return this.name.split(' ')[1];
   }
+
+
+  submitTripRequest() {
+    console.log(this.tripQuote);
+    this.apiController.postTripRequest(this.tripQuote);
+
+  }
+
 
 };
 
